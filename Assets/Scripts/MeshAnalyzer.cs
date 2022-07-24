141fe616d5f0783ecb36651e0677bcbf7885df4d
Tyler Beaupre <tbeaupre@wpi.edu>
2022-07-24 11:19:09 -0400

    Adds IsSymmetrical check

--- Assets/Scripts/MeshAnalyzer.cs
+++ Assets/Scripts/MeshAnalyzer.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MeshAnalyzer : MonoBehaviour
 {
     public MeshFilter meshFilterPrefab;
-
-    private const float PRECISION_ERROR = 0.0001f;
+    public static readonly float PRECISION_ERROR = 0.0001f;
 
     private Dictionary<string, FaceData> meshFaceDataMap = new Dictionary<string, FaceData>();
     private Vector3 centroid = (new Vector3(0.86603f, 0, -1.50f) + new Vector3(1.7321f, 0, 0)) / 3;
@@ -31,23 +31,23 @@ public class MeshAnalyzer : MonoBehaviour
         Debug.Log(meshes.Count);
         foreach (Mesh m in meshes)
         {
-            FaceData faceData = new FaceData();
+            FaceData faceData = new FaceData(m.name);
 
             for (int i = 0; i < m.vertexCount; ++i)
             {
                 Vector3 v = m.vertices[i];
                 Debug.Log($"{m.name}: {v}");
                 if (Mathf.Abs(v.z) < PRECISION_ERROR)
-                    faceData.backFace.Add(v - sideOffset);
+                    faceData.backFace.Add(new Vertex(v - sideOffset));
                 if (Mathf.Abs(v.y - 1.0f) < PRECISION_ERROR)
                 {
-                    Vector3 centeredVertex = v - centroid;
-                    faceData.topFace.Add(new Vector2(centeredVertex.x, centeredVertex.z)); // Want the position relative to centroid so it can easily be checked for rotational symmetry
+                    Vector3 centeredVertex = v - centroid; // Want the position relative to centroid so it can easily be checked for rotational symmetry
+                    faceData.topFace.Add(new Vertex(new Vector2(centeredVertex.x, centeredVertex.z)));
                 }
                 if (Mathf.Abs(v.y) < PRECISION_ERROR)
                 {
-                    Vector3 centeredVertex = v - centroid;
-                    faceData.bottomFace.Add(new Vector2(centeredVertex.x, centeredVertex.z));
+                    Vector3 centeredVertex = v - centroid; // Want the position relative to centroid so it can easily be checked for rotational symmetry
+                    faceData.bottomFace.Add(new Vertex(new Vector2(centeredVertex.x, centeredVertex.z)));
                 }
             }
 
@@ -56,7 +56,7 @@ public class MeshAnalyzer : MonoBehaviour
             {
                 Vector3 v = rotatedVertices[i];
                 if (Mathf.Abs(v.z) < PRECISION_ERROR)
-                    faceData.leftFace.Add(v - sideOffset);
+                    faceData.leftFace.Add(new Vertex(v - sideOffset));
             }
 
             rotatedVertices = RotateVertices(rotatedVertices);
@@ -64,10 +64,11 @@ public class MeshAnalyzer : MonoBehaviour
             {
                 Vector3 v = rotatedVertices[i];
                 if (Mathf.Abs(v.z) < PRECISION_ERROR)
-                    faceData.rightFace.Add(v - sideOffset);
+                    faceData.rightFace.Add(new Vertex(v - sideOffset));
             }
 
             meshFaceDataMap[m.name] = faceData;
+            Debug.Log($"{m.name}: {faceData.IsSymmetrical()}");
         }
     }
 
@@ -89,18 +90,44 @@ public class MeshAnalyzer : MonoBehaviour
 
 public class FaceData
 {
-    public HashSet<Vector2> backFace;
-    public HashSet<Vector2> rightFace;
-    public HashSet<Vector2> leftFace;
-    public HashSet<Vector2> topFace;
-    public HashSet<Vector2> bottomFace;
+    public string meshName;
+    public SortedSet<Vertex> backFace;
+    public SortedSet<Vertex> rightFace;
+    public SortedSet<Vertex> leftFace;
+    public SortedSet<Vertex> topFace;
+    public SortedSet<Vertex> bottomFace;
 
-    public FaceData()
+    public FaceData(string meshName)
     {
-        backFace = new HashSet<Vector2>();
-        rightFace = new HashSet<Vector2>();
-        leftFace = new HashSet<Vector2>();
-        topFace = new HashSet<Vector2>();
-        bottomFace = new HashSet<Vector2>();
+        this.meshName = meshName;
+        backFace = new SortedSet<Vertex>();
+        rightFace = new SortedSet<Vertex>();
+        leftFace = new SortedSet<Vertex>();
+        topFace = new SortedSet<Vertex>();
+        bottomFace = new SortedSet<Vertex>();
+    }
+
+    public bool IsSymmetrical()
+    {
+        return AreFacesEqual(backFace, rightFace) && AreFacesEqual(rightFace, leftFace);
+    }
+
+    private bool AreFacesEqual(SortedSet<Vertex> face1, SortedSet<Vertex> face2)
+    {
+        if (face1.Equals(face2))
+            return true;
+        if (face1.Count != face2.Count)
+            return false;
+
+        // This is pretty gross, but the floating point precision errors were making .SetEquals() a non-option
+        List<Vertex> list1 = new List<Vertex>(face1);
+        List<Vertex> list2 = new List<Vertex>(face2);
+        for (int i = 0; i < list1.Count; ++i)
+        {
+            if (list1[i] != list2[i])
+                return false;
+        }
+
+        return true;
     }
 }
---
+++ Assets/Scripts/Vertex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public struct Vertex : IComparable<Vertex>
+{
+    public Vector2 position;
+
+    public Vertex(Vector2 position)
+    {
+        this.position = position;
+    }
+
+    public static bool operator ==(Vertex v1, Vertex v2)
+    {
+        return Mathf.Abs(v1.position.x - v2.position.x) < MeshAnalyzer.PRECISION_ERROR &&
+            Mathf.Abs(v1.position.y - v2.position.y) < MeshAnalyzer.PRECISION_ERROR;
+    }
+
+    public static bool operator !=(Vertex v1, Vertex v2)
+    {
+        return Mathf.Abs(v1.position.x - v2.position.x) >= MeshAnalyzer.PRECISION_ERROR ||
+            Mathf.Abs(v1.position.y - v2.position.y) >= MeshAnalyzer.PRECISION_ERROR;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Vertex v = (Vertex)obj;
+        return this == v;
+    }
+
+    public override int GetHashCode()
+    {
+        return position.GetHashCode();
+    }
+
+    public int CompareTo(Vertex other)
+    {
+        if (position.x.CompareTo(other.position.x) != 0)
+            return position.x.CompareTo(other.position.x);
+        else
+            return position.y.CompareTo(other.position.y);
+    }
+}
